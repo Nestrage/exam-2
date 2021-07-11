b0701be5ecc0b29d9381a39c321eb872c6845d19
Nestrage <dimakoujav@gmail.com>
2021-07-11 17:53:06 +0300

    ну вот как то так
    сделал все задания по JS, не успел доверстать

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
+$(document).ready(function () {
+  $(".header__slick").slick({ arrows: false, dots: true });
+});
+
+$(document).ready(function () {
+  $(".news__slick").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
     document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 }
